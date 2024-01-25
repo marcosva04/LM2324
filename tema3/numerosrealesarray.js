3da052209019d9a83ba968c11146b67bbf9bc7e9
Marcos Vicente Amián <marcosvicente04@iesamachado.org>
2024-01-25 14:29:15 +0100

    comienzo numeros reales en array

---
+++ tema3/numerosrealesarray.js
@@ -0,0 +1,11 @@
+function anadirElemento(){
+    const nuevoElemento = document.getElementById('anadirElemento').value;
+    if (nuevoElemento.trim() !== '') {
+      array.push(nuevoElemento);
+    }
+}
+
+
+function sumar(){
+
+}
